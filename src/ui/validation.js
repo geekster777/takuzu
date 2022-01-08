4aea6dec66c733f51cdbba278758d813faca245d
Nick Hilton <nhilton777@gmail.com>
2022-01-08 01:58:27 -0500

    Dedupe rows and columns in validator

--- src/ui/validation.js
+++ src/ui/validation.js
@@ -6,8 +6,8 @@ export const TILE_STATE = {
 
 export function isValid(board) {
   const rows = board.map((row, i) => row.map((tileState, j) => ({id: `${i},${j}`, state: tileState})));
-  const columns = rows.map((_, i) => rows.map(row => row[i]));
-  const groups = [...rows, ...columns];
+  const cols = rows.map((_, i) => rows.map(row => row[i]));
+  const groups = [...rows, ...cols];
 
   const unselectedTiles = rows.flat().filter(tile => tile.state == TILE_STATE.UNSELECTED);
   if (unselectedTiles.length !== 0) {
@@ -28,6 +28,31 @@ export function isValid(board) {
         // 3 consecutive tiles
         return {failure: 'consecutive', invalidTiles: [group[i].id, group[i+1].id, group[i+2].id]};
       }
+    }
+  }
+
+  const rowIds = {};
+  for (let row of rows) {
+    const rowId = row.map(tile => tile.state).join('');
+    if (rowIds[rowId] != null) {
+      const invalidTiles = [
+        ...row.map(tile => tile.id),
+        ...rowIds[rowId].map(tile => tile.id),
+      ];
+      return {failure: 'identical_rows', invalidTiles};
+    }
+    rowIds[rowId] = row;
+  }
+
+  const colIds = {};
+  for (let col of cols) {
+    const colId = col.map(tile => tile.state).join('');
+    if (colIds[colId] != null) {
+      const invalidTiles = [
+        ...col.map(tile => tile.id),
+        ...colIds[colId].map(tile => tile.id),
+      ];
+      return {failure: 'identical_cols', invalidTiles};
     }
   }
 
